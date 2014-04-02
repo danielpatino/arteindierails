42f81c879de50147477638334e8f2e59a9ffdca4
Daniel Patino <danpatino89@gmail.com>
2014-04-02 18:43:11 -0300

    arteindierails start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arteindierails::Application.config.secret_key_base = '73843fc5a16bd93736bbc132a0b97041bf4fad63d32d8516dfc5dac5099bcd465b338ae4e5a6b297126b32160173379438d9e4784a2aebd58e202edf89cd4e44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Arteindierails::Application.config.session_store :cookie_store, key: '_arteindierails_session'
